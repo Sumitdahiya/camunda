79a949a9ec4e156cd149124c2413779ceca98bbf
Valentin Vago <zeropaper@irata.ch>
2014-02-13 10:47:21 +0100

    linting

--- webapps/camunda-webapp/webapp/src/main/webapp/app/common/directives/email.js
+++ webapps/camunda-webapp/webapp/src/main/webapp/app/common/directives/email.js
@@ -1,18 +1,20 @@
-ngDefine('camunda.common.directives', [ 'jquery' ], function(module, $) {
-
+/* global ngDefine: false */
+ngDefine('camunda.common.directives.email', [], function(module) {
+  'use strict';
   var Directive = function() {
     return {
       restrict: 'A',
       require: 'ngModel',
       link: function (scope, element, attrs, model) {
-        
+
         var EMAIL_REGEX = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        
+
         model.$parsers.unshift(function(viewValue) {
-          if (EMAIL_REGEX.test(viewValue) || viewValue=='') {
+          if (EMAIL_REGEX.test(viewValue) || viewValue === '') {
             model.$setValidity('email', true);
             return viewValue;
-          } else {
+          }
+          else {
             model.$setValidity('email', false);
             return null;
           }
@@ -21,6 +23,6 @@ ngDefine('camunda.common.directives', [ 'jquery' ], function(module, $) {
     };
   };
 
-  module.directive("email", Directive);
+  module.directive('email', Directive);
 
 });
