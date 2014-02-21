766e25ad9c646db007ad181f598653a6070c2d33
Michael Schoettes <michael.schoettes@camunda.com>
2014-02-21 15:16:02 +0100

    chore(integration-test): clean up folder

--- qa/integration-tests-webapps/src/test/javascript/test-setup-scripts/start-test-setup.bat
+++
@@ -1,7 +0,0 @@
-@ECHO ON
-
-START /min "Start webdriver" webdriver-runner.bat 
-
-START /min "Start camunda BPM Platform" camunda-bpm-runner.bat
-
-START /max "Protractor Runner" protractor-runner.bat
