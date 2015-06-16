8cb50ee2646b0a74d27adf04ec18a932898f837d
Thorben Lindhauer <thorben.lindhauer@camunda.com>
2015-06-16 16:44:26 +0200

    fix(engine): use job rejection handler in platform executor

--- engine/src/main/java/org/camunda/bpm/engine/impl/jobexecutor/RuntimeContainerJobExecutor.java
+++ engine/src/main/java/org/camunda/bpm/engine/impl/jobexecutor/RuntimeContainerJobExecutor.java
@@ -1,9 +1,9 @@
 /* Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,23 +20,23 @@ import org.camunda.bpm.engine.ProcessEngineException;
 import org.camunda.bpm.engine.impl.ProcessEngineImpl;
 
 /**
- * <p>JobExecutor implementation that delegates the execution of jobs 
+ * <p>JobExecutor implementation that delegates the execution of jobs
  * to the {@link RuntimeContainerDelegate RuntimeContainer}</p>
- * 
+ *
  * @author Daniel Meyer
  *
  */
 public class RuntimeContainerJobExecutor extends JobExecutor {
-  
+
   protected void startExecutingJobs() {
-    
+
     final RuntimeContainerDelegate runtimeContainerDelegate = getRuntimeContainerDelegate();
-    
+
     // schedule job acquisition
     if(!runtimeContainerDelegate.getExecutorService().schedule(acquireJobsRunnable, true)) {
       throw new ProcessEngineException("Could not schedule AcquireJobsRunnable for execution.");
     }
-   
+
   }
 
   protected void stopExecutingJobs() {
@@ -44,25 +44,22 @@ public class RuntimeContainerJobExecutor extends JobExecutor {
   }
 
   public void executeJobs(List<String> jobIds, ProcessEngineImpl processEngine) {
-    
+
     final RuntimeContainerDelegate runtimeContainerDelegate = getRuntimeContainerDelegate();
     final ExecutorService executorService = runtimeContainerDelegate.getExecutorService();
-    
+
     Runnable executeJobsRunnable = executorService.getExecuteJobsRunnable(jobIds, processEngine);
-    
+
     // delegate job execution to runtime container
     if(!executorService.schedule(executeJobsRunnable, false)) {
-      
-      // TODO: if the execution is rejected by the runtime container, execute the rejected jobs handler
 
-      // run in caller thread
-      executeJobsRunnable.run();
+      rejectedJobsHandler.jobsRejected(executeJobsRunnable, jobIds, processEngine);
     }
-           
+
   }
 
   protected RuntimeContainerDelegate getRuntimeContainerDelegate() {
     return RuntimeContainerDelegate.INSTANCE.get();
   }
-  
+
 }
