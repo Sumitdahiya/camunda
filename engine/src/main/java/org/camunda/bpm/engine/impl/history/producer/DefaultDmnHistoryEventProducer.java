9d3a376a54dd8bf3eec5377c3df1e478d5a8c858
Philipp Ossler <philipp.ossler@camunda.com>
2015-09-02 11:16:15 +0200

    feat(engine) add history for decision evaluation
    - create history producer
    
    related to CAM-4498

--- engine/src/main/java/org/camunda/bpm/engine/impl/history/producer/DefaultDmnHistoryEventProducer.java
+++ engine/src/main/java/org/camunda/bpm/engine/impl/history/producer/DefaultDmnHistoryEventProducer.java
@@ -1,9 +1,9 @@
 /* Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -14,14 +14,59 @@
 package org.camunda.bpm.engine.impl.history.producer;
 
 import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.impl.dmn.entity.repository.HistoricDecisionInstanceEntity;
 import org.camunda.bpm.engine.impl.history.event.HistoryEvent;
+import org.camunda.bpm.engine.impl.history.event.HistoryEventTypes;
+import org.camunda.bpm.engine.impl.persistence.entity.ExecutionEntity;
+import org.camunda.bpm.engine.impl.persistence.entity.ProcessDefinitionEntity;
+import org.camunda.bpm.engine.impl.util.ClockUtil;
 
+/**
+ * @author Philipp Ossler
+ */
 public class DefaultDmnHistoryEventProducer implements DmnHistoryEventProducer {
 
   @Override
   public HistoryEvent createDecisionEvaluatedEvt(DelegateExecution execution) {
-    // TODO Auto-generated method stub
-    return null;
+    final ExecutionEntity executionEntity = (ExecutionEntity) execution;
+
+    // create event instance
+    HistoricDecisionInstanceEntity event = newDecisionInstanceEventEntity(executionEntity);
+    // initialize event
+    initDecisionInstanceEvent(event, executionEntity, HistoryEventTypes.DMN_DECISION_EVALUATE);
+    // set current time as evaluation time
+    event.setEvaluationTime(ClockUtil.getCurrentTime());
+
+    return event;
   }
 
+  protected HistoricDecisionInstanceEntity newDecisionInstanceEventEntity(ExecutionEntity executionEntity) {
+    return new HistoricDecisionInstanceEntity();
+  }
+
+  protected void initDecisionInstanceEvent(HistoricDecisionInstanceEntity event, ExecutionEntity execution, HistoryEventTypes eventType) {
+    event.setEventType(eventType.getEventName());
+
+    // TODO set decision definition properties
+    // event.setDecisionDefinitionKey(decisionDefinitionKey);
+    // event.setDecisionDefinitionName(decisionDefinitionName);
+
+    event.setProcessDefinitionKey(getProcessDefinitionKey(execution));
+    event.setProcessDefinitionId(execution.getProcessDefinitionId());
+
+    event.setProcessInstanceId(execution.getProcessInstanceId());
+    event.setExecutionId(execution.getId());
+
+    event.setActivityId(execution.getActivityId());
+    event.setActivityInstanceId(execution.getActivityInstanceId());
+  }
+
+  protected String getProcessDefinitionKey(ExecutionEntity execution) {
+    ProcessDefinitionEntity definition = (ProcessDefinitionEntity) execution.getProcessDefinition();
+    if (definition != null) {
+      return definition.getKey();
+    } else {
+      return null;
+    }
+  }
 }
